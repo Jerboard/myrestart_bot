02cc679253573e867f399f6a09e7e0aaa968084f
dgush <dgushch@gmail.com>
2024-03-08 18:03:17 +0400

    0.2.7

--- handlers/diary_stress.py
+++ handlers/diary_stress.py
@@ -5,7 +5,7 @@ import os
 
 import db
 import keyboards as kb
-from init import dp, TZ
+from init import dp, TZ, log_error
 from utils.cover_photos import get_cover_photo
 from utils.plots import get_global_stress_plot, get_daily_stress_plot
 from enums import DiaryCB
@@ -63,6 +63,8 @@ async def account_start(cb: CallbackQuery):
 async def account_start(cb: CallbackQuery):
     _, plot_type = cb.data.split (':')
 
+    log_error(f'plot_type: {plot_type}')
+
     if plot_type == 'global':
         global_plot_data = await db.get_global_stress_data(cb.from_user.id)
         if not global_plot_data.happy and not global_plot_data.unhappy:
@@ -70,6 +72,7 @@ async def account_start(cb: CallbackQuery):
             return
 
         else:
+            log_error (f'start_global')
             bit = 1200 / (global_plot_data.happy + global_plot_data.unhappy)
             happy_count = round(global_plot_data.happy * bit)
             unhappy_count = round(global_plot_data.unhappy * bit)
@@ -78,24 +81,29 @@ async def account_start(cb: CallbackQuery):
             text = 'График состояния за весь период'
 
             cached_plot = await db.get_plot_cache(type_=plot_type, comment=plot_ident)
+            log_error (f'cached_plot: {cached_plot}')
             if cached_plot:
                 photo = InputMediaPhoto (media=cached_plot.file_id, caption=text)
                 await cb.message.edit_media (media=photo, reply_markup=kb.get_archive_stress_kb (plot_type))
 
             else:
+                log_error (f'get_global_stress_plot')
                 get_global_stress_plot(
                     user_id=cb.from_user.id,
                     happy=happy_count,
                     unhappy=unhappy_count
                 )
+                log_error (f'get_global_stress_plot end')
                 photo_path = os.path.join ('temp', f'{plot_type}_{cb.from_user.id}.jpg')
                 photo_input = FSInputFile (photo_path)
+                log_error (f'photo_path: {photo_path}')
 
                 photo = InputMediaPhoto (media=photo_input, caption=text)
                 sent = await cb.message.edit_media (media=photo, reply_markup=kb.get_archive_stress_kb (plot_type))
                 await db.add_plot_in_cache_global(type_=plot_type, comment=plot_ident, file_id=sent.photo[-1].file_id)
 
                 os.remove(photo_path)
+                log_error (f'end')
 
     else:
         text = 'График состояния по дням'
